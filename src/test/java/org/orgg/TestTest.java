ff1dc6c2e42f1573332218314339bc20418e14c6
Atchaya-dotcom <atchayan.pixelweb@gmail.com>
2026-04-21 12:24:17 +0530

    My second commit

---
+++ src/test/java/org/orgg/TestTest.java
@@ -0,0 +1,18 @@
+package org.orgg;
+
+public class TestTest {
+
+	public void sample() {
+		
+		System.out.println("Atchaya");
+
+	}
+	
+	public static void main (String[]args) {
+		
+		TestTest ts=new TestTest();
+		
+		ts.sample();
+		
+	}
+}
