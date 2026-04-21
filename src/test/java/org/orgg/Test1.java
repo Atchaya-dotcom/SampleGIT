32904cc08038e201304833880a9468f6ab9662c5
Atchaya-dotcom <atchayan.pixelweb@gmail.com>
2026-04-21 16:20:15 +0530

    Committing Test1 to BranchX

---
+++ src/test/java/org/orgg/Test1.java
@@ -0,0 +1,15 @@
+package org.orgg;
+
+public class Test1 {
+	
+	public void sample1() {
+		
+		System.out.println("Tester");
+	}
+	
+	public static void main (String[]args) {
+		
+		Test1 ts=new Test1();
+	}
+
+}
