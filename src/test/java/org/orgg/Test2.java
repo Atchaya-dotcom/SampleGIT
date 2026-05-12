510b1227611944460069a807b4e20bb4e852121a
Atchaya-dotcom <atchayan.pixelweb@gmail.com>
2026-05-12 12:02:02 +0530

    Add @test

--- src/test/java/org/orgg/Test2.java
+++ src/test/java/org/orgg/Test2.java
@@ -1,15 +1,16 @@
 package org.orgg;
 
+import org.testng.annotations.Test;
+
 public class Test2 {
 
+	
+@Test
 public void sample1() {
 		
 		System.out.println("Tester1");
 	}
 	
-	public static void main (String[]args) {
-		
-		Test2 ts=new Test2();
-	}
+	
 	
 }
