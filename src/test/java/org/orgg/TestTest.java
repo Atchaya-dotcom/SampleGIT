948bda0d104add1bb86c0c357cb99d47c05e9b19
Atchaya-dotcom <atchayan.pixelweb@gmail.com>
2026-05-18 17:44:07 +0530

    Clean project setup with ignored reports

--- src/test/java/org/orgg/TestTest.java
+++ src/test/java/org/orgg/TestTest.java
@@ -1,18 +1,21 @@
 package org.orgg;
 
+import org.testng.annotations.Test;
+
 public class TestTest {
 
+	@Test
 	public void sample() {
 		
 		System.out.println("Atchaya");
 
 	}
 	
-	public static void main (String[]args) {
-		
-		TestTest ts=new TestTest();
-		
-		ts.sample();
-		
-	}
+//	public static void main (String[]args) {
+//		
+//		TestTest ts=new TestTest();
+//		
+//		ts.sample();
+//		
+//	}
 }
